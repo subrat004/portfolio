fb54cc4bb6b9e5ffa4577787b6ad96f549725650
SUBRAT <subratdhapola007@gmail.com>
2023-09-26 12:25:54 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,271 @@
+import {
+	mobile,
+	backend,
+	creator,
+	web,
+	javascript,
+	typescript,
+	html,
+	css,
+	reactjs,
+	redux,
+	tailwind,
+	nodejs,
+	mongodb,
+	git,
+	figma,
+	docker,
+	meta,
+	starbucks,
+	corizo,
+	sdlogo,
+	tesla,
+	shopify,
+	carrent,
+	cryptoapp,
+	itsolutionwebsite,
+	videohubapp,
+	jobit,
+	tripguide,
+	threejs,
+} from '../assets';
+
+export const navLinks = [
+	{
+		id: 'about',
+		title: 'About',
+	},
+	{
+		id: 'work',
+		title: 'Work',
+	},
+	{
+		id: 'contact',
+		title: 'Contact',
+	},
+];
+
+const services = [
+	{
+		title: 'Web Developer',
+		icon: web,
+	},
+	{
+		title: 'React Js Developer',
+		icon: mobile,
+	},
+	{
+		title: 'Backend Developer',
+		icon: backend,
+	},
+	// {
+	// 	title: 'Content Creator',
+	// 	icon: creator,
+	// },
+];
+
+const technologies = [
+	{
+		name: 'HTML 5',
+		icon: html,
+	},
+	{
+		name: 'CSS 3',
+		icon: css,
+	},
+	{
+		name: 'JavaScript',
+		icon: javascript,
+	},
+	{
+		name: 'TypeScript',
+		icon: typescript,
+	},
+	{
+		name: 'React JS',
+		icon: reactjs,
+	},
+	{
+		name: 'Redux Toolkit',
+		icon: redux,
+	},
+	{
+		name: 'Tailwind CSS',
+		icon: tailwind,
+	},
+	{
+		name: 'Node JS',
+		icon: nodejs,
+	},
+	{
+		name: 'MongoDB',
+		icon: mongodb,
+	},
+	{
+		name: 'Three JS',
+		icon: threejs,
+	},
+	{
+		name: 'git',
+		icon: git,
+	},
+	{
+		name: 'figma',
+		icon: figma,
+	},
+	{
+		name: 'docker',
+		icon: docker,
+	},
+];
+
+const experiences = [
+	{
+		title: 'Freelance React Native  And React.js Developer',
+		company_name: 'Freelancer',
+		icon: sdlogo,
+		iconBg: 'black',
+		date: 'Jan 2021 - Feb 2022',
+		points: [
+			'Developing and maintaining web applications using React.js and other related technologies.',
+			'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+			'Implementing responsive design and ensuring cross-browser compatibility.',
+			'Participating in code reviews and providing constructive feedback to other developers.',
+		],
+	},
+	{
+		title: 'React.js Developer',
+		company_name: 'Corizo',
+		icon: corizo,
+		iconBg: 'white',
+		date: 'March 2023 - May 2023',
+		points: [
+			'Developing and maintaining web applications using React.js and other related technologies.',
+			'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+			'Implementing responsive design and ensuring cross-browser compatibility.',
+			'Participating in code reviews and providing constructive feedback to other developers.',
+		],
+	},
+
+	// {
+	// 	title: 'Web Developer',
+	// 	company_name: 'Shopify',
+	// 	icon: shopify,
+	// 	iconBg: '#383E56',
+	// 	date: 'Jan 2022 - Jan 2023',
+	// 	points: [
+	// 		'Developing and maintaining web applications using React.js and other related technologies.',
+	// 		'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+	// 		'Implementing responsive design and ensuring cross-browser compatibility.',
+	// 		'Participating in code reviews and providing constructive feedback to other developers.',
+	// 	],
+	// },
+	// {
+	// 	title: 'Full stack Developer',
+	// 	company_name: 'Meta',
+	// 	icon: meta,
+	// 	iconBg: '#E6DEDD',
+	// 	date: 'Jan 2023 - Present',
+	// 	points: [
+	// 		'Developing and maintaining web applications using React.js and other related technologies.',
+	// 		'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+	// 		'Implementing responsive design and ensuring cross-browser compatibility.',
+	// 		'Participating in code reviews and providing constructive feedback to other developers.',
+	// 	],
+	// },
+];
+
+const testimonials = [
+	{
+		testimonial:
+			'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
+		name: 'Sara Lee',
+		designation: 'CFO',
+		company: 'Acme Co',
+		image: 'https://randomuser.me/api/portraits/women/4.jpg',
+	},
+	{
+		testimonial:
+			"I've never met a web developer who truly cares about their clients' success like Rick does.",
+		name: 'Chris Brown',
+		designation: 'COO',
+		company: 'DEF Corp',
+		image: 'https://randomuser.me/api/portraits/men/5.jpg',
+	},
+	{
+		testimonial:
+			"After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+		name: 'Lisa Wang',
+		designation: 'CTO',
+		company: '456 Enterprises',
+		image: 'https://randomuser.me/api/portraits/women/6.jpg',
+	},
+];
+
+const projects = [
+	{
+		name: 'Crypto Application',
+		description:
+			'Web-based platform that allows users to search various crypto currencies with their current rates in inr, usd and euro.',
+		tags: [
+			{
+				name: 'react',
+				color: 'blue-text-gradient',
+			},
+			{
+				name: 'mongodb',
+				color: 'green-text-gradient',
+			},
+			{
+				name: 'tailwind',
+				color: 'pink-text-gradient',
+			},
+		],
+		image: cryptoapp,
+		source_code_link: 'https://github.com/subrat004/CryptoApp',
+	},
+	{
+		name: ' IT Solution Website ',
+		description:
+			'Web application that enables users to search for job openings, view estimated salary ranges for positions, and various serivces provided by the websites .',
+		tags: [
+			{
+				name: 'react',
+				color: 'blue-text-gradient',
+			},
+			{
+				name: 'restapi',
+				color: 'green-text-gradient',
+			},
+			{
+				name: 'scss',
+				color: 'pink-text-gradient',
+			},
+		],
+		image: itsolutionwebsite,
+		source_code_link: 'https://github.com/subrat004/project1',
+	},
+	{
+		name: 'Video Hub',
+		description:
+			'A comprehensive video hub platform that enables the user to watch , upload and download the videos .',
+		tags: [
+			{
+				name: 'nextjs',
+				color: 'blue-text-gradient',
+			},
+			{
+				name: 'supabase',
+				color: 'green-text-gradient',
+			},
+			{
+				name: 'css',
+				color: 'pink-text-gradient',
+			},
+		],
+		image: videohubapp,
+		source_code_link: 'https://github.com/',
+	},
+];
+
+export { services, technologies, experiences, testimonials, projects };
